f77a1f86729b346278d766ba5e1d757dde283eb5
Shravya270 <shravyashetty246@gmail.com>
2025-10-21 22:58:27 +0530

    implemented crud operations and other stuff

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const apiRoutes = require('./routes');
 const serverConfig = require('./config/server-config');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.use('/api',apiRoutes);
 
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const {InfoController} = require('../../controllers');
+const airplaneRoutes = require('./airplane-router');
 const router = express.Router();
 
+router.use('/airplanes',airplaneRoutes)
 router.get('/info',InfoController.info)
 
 
